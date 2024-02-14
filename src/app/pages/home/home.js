a39f22ce77fa2ef8f23f841553a4f7c35ef29985
rydelldonato <rydell.donato@gmail.com>
2024-02-13 18:29:52 -0800

    added homepage header + styling

---
+++ src/app/pages/home/home.js
@@ -0,0 +1,14 @@
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="mainContainer">
+      <div className="header">
+        <h1>ecode qr story hub</h1>
+      </div>
+      <div className="navbar">
+        {/* insert navbar component here */}
+        </div>
+    </div>
+  );
+}
