408a147fad7e382fd6eea87925f4628de3f59e1c
dylanmclane <mclane_dylan@yahoo.com>
2024-02-26 12:11:38 -0800

    Updated styling added hamburger menu below 768px reorganized class selectors updated comments

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './navbar.css'
+// To enable client-side state management in the navbar (note: for responsiveness like tracking when the mobile menu is open or closed)
+'use client';
+
+// Imports
+import React, { useState } from 'react';
+import './navbar.css';
 import Link from 'next/link';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+  
     return (
       <nav className="navbar">
-        <ul className="navbar-list">
-          <li className="navbar-item">Home</li>
+        <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+            <div>{isOpen ? 'X' : '☰'}</div>
+        </div>
+        <ul className={`navbar-list ${isOpen ? 'open': ''}`}>
+          <li className="navbar-item"><Link href='https://ecodeofficial.com/'>Home</Link></li>
           <li className="navbar-item"><Link href='/story1'>Story 1</Link></li>
           <li className="navbar-item"><Link href='/story2'>Story 2</Link></li>
           <li className="navbar-item"><Link href='/story3'>Story 3</Link></li>
