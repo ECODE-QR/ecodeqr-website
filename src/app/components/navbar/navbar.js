1f31c0ff7188582a8b844b15c746fe1c919dc21f
Jason Johnson <jas2johns@gmail.com>
2024-03-20 16:50:29 -0700

    implemented nav styling for mobile and desktop

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,25 +1,22 @@
-// To enable client-side state management in the navbar (note: for responsiveness like tracking when the mobile menu is open or closed)
-'use client';
-
-// Imports
-import React, { useState } from 'react';
+import React from 'react';
 import './navbar.css';
 import Link from 'next/link';
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    return (
-      <nav className="navbar">
-        <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
-            <div>{isOpen ? 'X' : '☰'}</div>
-        </div>
-        <ul className={`navbar-list ${isOpen ? 'open': ''}`}>
-          <li className="navbar-item"><Link href='https://ecodeofficial.com/'>Home</Link></li>
-          <li className="navbar-item"><Link href='/story1'>Story 1</Link></li>
-          <li className="navbar-item"><Link href='/story2'>Story 2</Link></li>
-          <li className="navbar-item"><Link href='/story3'>Story 3</Link></li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar nav-container">
+      <input type="checkbox" className="checkbox" id="menu-toggle" />
+      <label htmlFor="menu-toggle" className="hamburger-lines">
+        <span className="line line1"></span>
+        <span className="line line2"></span>
+        <span className="line line3"></span>
+      </label>
+      <ul className="menu-items">
+        <li className="navbar-item"><Link href='https://ecodeofficial.com/'>Home</Link></li>
+        <li className="navbar-item"><Link href='/story1'>Story 1</Link></li>
+        <li className="navbar-item"><Link href='/story2'>Story 2</Link></li>
+        <li className="navbar-item"><Link href='/story3'>Story 3</Link></li>
+      </ul>
+    </nav>
+  );
+}
