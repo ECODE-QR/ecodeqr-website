501780e0ee32a09f0254ceea3a335e4451bd53cc
vgalante2 <vgalante0@gmail.com>
2024-03-20 19:26:12 -0400

    added story 2 and 3

--- src/app/components/slideshow/slideShowFunction/slideShowFunction.js
+++ src/app/components/slideshow/slideShowFunction/slideShowFunction.js
@@ -48,7 +48,7 @@ const Image = styled.img`
   max-height: 90%;
 `;
 
-const Story = ({ images }) => {
+const Story = ({ images, renaissanceImages }) => {
   const [index, setIndex] = useState(0);
   const [progress, setProgress] = useState(0);
   const imageDisplayDuration = 8000;
@@ -75,7 +75,7 @@ const Story = ({ images }) => {
     setProgress(0);
   };
 
-  
+
 
   return (
     <StoryContainer onClick={handleClick}>
