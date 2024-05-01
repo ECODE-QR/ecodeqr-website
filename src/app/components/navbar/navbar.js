d706594657855c0ce0c0c6897a51372671f8b6ad
rydelldonato <rydell.donato@gmail.com>
2024-05-01 15:31:24 -0700

    added Jason's navbar styling and Vincent's Stories

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -3,7 +3,6 @@ import './navbar.css';
 import Link from 'next/link';
 
 export default function Navbar() {
-<<<<<<< HEAD
   return (
     <nav className="navbar nav-container">
       <input type="checkbox" className="checkbox" id="menu-toggle" />
@@ -17,27 +16,9 @@ export default function Navbar() {
         <li className="navbar-item"><Link href='/story1'>Story 1</Link></li>
         <li className="navbar-item"><Link href='/story2'>Story 2</Link></li>
         <li className="navbar-item"><Link href='/story3'>Story 3</Link></li>
+        <li className="navbar-item"><Link href='/story4'>Story 4</Link></li>
+          <li className="navbar-item"><Link href='/story5'>Story 5</Link></li>
       </ul>
     </nav>
   );
 }
-=======
-    const [isOpen, setIsOpen] = useState(false);
-  
-    return (
-      <nav className="navbar">
-        <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
-            <div>{isOpen ? 'X' : '☰'}</div>
-        </div>
-        <ul className={`navbar-list ${isOpen ? 'open': ''}`}>
-          <li className="navbar-item"><Link href='https://ecodeofficial.com/'>Home</Link></li>
-          <li className="navbar-item"><Link href='/story1'>Story 1</Link></li>
-          <li className="navbar-item"><Link href='/story2'>Story 2</Link></li>
-          <li className="navbar-item"><Link href='/story3'>Story 3</Link></li>
-          <li className="navbar-item"><Link href='/story4'>Story 4</Link></li>
-          <li className="navbar-item"><Link href='/story5'>Story 5</Link></li>
-        </ul>
-      </nav>
-    );
-  }
->>>>>>> 5557b41 (fixed stories)
