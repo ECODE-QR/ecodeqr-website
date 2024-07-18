01eafae7a87f18e26f4e94d612c4643a1114dec7
vgalante2 <vgalante0@gmail.com>
2024-07-18 17:21:19 -0400

    fixed nav/hero

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,18 +1,24 @@
-"use client";
+'use client';
 
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import Link from 'next/link';
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    return (
-      <nav className="navbar">
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
         <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
-            <div>{isOpen ? 'X' : '☰'}</div>
+          <div>{isOpen ? 'X' : '☰'}</div>
         </div>
-        <ul className={`navbar-list ${isOpen ? 'open': ''}`}>
+        <ul className={`navbar-list ${isOpen ? 'open' : ''}`}>
+          {isOpen && (
+            <li className="close-btn" onClick={() => setIsOpen(false)}>
+              X
+            </li>
+          )}
           <li className="navbar-item"><Link href='https://ecodeofficial.com/'>Home</Link></li>
           <li className="navbar-item"><Link href='/story1'>Story 1</Link></li>
           <li className="navbar-item"><Link href='/story2'>Story 2</Link></li>
@@ -20,6 +26,7 @@ export default function Navbar() {
           <li className="navbar-item"><Link href='/story4'>Story 4</Link></li>
           <li className="navbar-item"><Link href='/story5'>Story 5</Link></li>
         </ul>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
+}
