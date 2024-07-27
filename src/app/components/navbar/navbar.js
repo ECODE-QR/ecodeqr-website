cf5b5e275bb0d3fe670ddec0c257ba54fb2019c3
vgalante2 <vgalante0@gmail.com>
2024-07-27 13:54:46 -0400

    fixed nav

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -7,14 +7,17 @@ import './navbar.css';
 import Link from 'next/link';
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
+
   
     return (
-      <nav className="navbar">
-        <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
-            <div>{isOpen ? 'X' : '☰'}</div>
-        </div>
-        <ul className={`navbar-list ${isOpen ? 'open': ''}`}>
+      <nav className="navbar nav-container">
+        <input type="checkbox" className="checkbox" id="menu-toggle" />
+        <label htmlFor="menu-toggle" className="hamburger-lines">
+          <span className="line line1"></span>
+          <span className="line line2"></span>
+          <span className="line line3"></span>
+        </label>
+        <ul className="menu-items">
           <li className="navbar-item"><Link href='https://ecodeofficial.com/'>Home</Link></li>
           <li className="navbar-item"><Link href='/story1'>Story 1</Link></li>
           <li className="navbar-item"><Link href='/story2'>Story 2</Link></li>
